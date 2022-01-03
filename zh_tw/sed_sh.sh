b938560998c6b460227b815453f6dab78515fc0b
descent <descent0@gmail.com>
2022-01-03 10:14:44 +0800

    new file:   sed_sh.sh

---
+++ zh_tw/sed_sh.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+sed -i -e 's/：/: /g' *
+sed -i -e 's/，/, /g' *
+sed -i -e 's/）/) /g' *
+sed -i -e 's/（/ (/g' *
+sed -i -e 's/？/? /g' *
+sed -i -e 's/；/; /g' *
+sed -i -e 's/！/! /g' *
+sed -i -e 's/……/....../g' *
+sed -i -e 's/內存/記憶體/g' *
+sed -i -e 's/字節/ byte /g' *
+sed -i -e 's/喬布斯/賈伯斯/g' *
+sed -i -e 's/黑客/駭客/g' *
+sed -i -e 's/出棧/pop 出堆疊/g' *
+sed -i -e 's/入棧/push 到堆疊/g' *
+sed -i -e 's/常量/常數/g' *
+sed -i -e 's/這斷/這段/g' *
+sed -i -e 's/指針/指標/g' *
+sed -i -e 's/支持/支援/g' *
+sed -i -e 's/枚舉/列舉/g' *
+sed -i -e 's/操作系統/作業系統/g' *
+sed -i -e 's/宏/macro /g' *
+sed -i -e 's/代碼/程式碼/g' *
+sed -i -e 's/調用/呼叫/g' *
+sed -i -e 's/遞歸/遞迴/g' *
+sed -i -e 's/數組/陣列/g' *
+sed -i -e 's/變量/變數/g' *
+sed -i -e 's/彙編/組合語言/g' *
+sed -i -e 's/寄存/暫存/g' *
+sed -i -e 's/堆棧/堆疊/g' *
+sed -i -e 's/棧/堆疊/g' *
+sed -i -e 's/堆上/ heap 上/g' *
+sed -i -e 's/數據/資料/g' *
+sed -i -e 's/地址/位址/g' *
+sed -i -e 's/程序員/程式員/g' *
+sed -i -e 's/程序/程式/g' *
+sed -i -e 's/關係資料庫/關聯式資料庫/g' *
+sed -i -e 's/程序代碼/程式碼/g' *
+sed -i -e 's/可視化/視覺化/g' *
+sed -i -e 's/面向對象/物件導向/g' *
+sed -i -e 's/數據庫/資料庫/g' *
+sed -i -e 's/軟件/軟體/g' *
+sed -i -e 's/硬件/硬體/g' *
+sed -i -e 's/信息/訊息/g' *
+sed -i -e 's/源碼/原始碼/g' *
+sed -i -e 's/網絡/網路/g' *
+sed -i -e 's/示例/範例/g' *
+sed -i -e 's/文件/檔案/g' *
+sed -i -e 's/文檔/文件/g' *
+sed -i -e 's/用戶/使用者/g' *
+sed -i -e 's/算法/演算法/g' *
+sed -i -e 's/調試/除錯/g' *
+sed -i -e 's/施樂/全錄/g' *
+sed -i -e 's/標準程式庫/標準程式庫/g' *
+sed -i -e 's/程式庫/程式庫/g' *
+#sed -i -e 's/[A-Za-z]*//g' *
+
